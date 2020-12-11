dff1acdd6c0edbd93388125576eec2eb6c05353d
OrdinaryWizard <75456880+OrdinaryWizard@users.noreply.github.com>
2020-12-11 19:03:52 +0800

    Added new functions that make input easier to read

--- equation-solver.py
+++ equation-solver.py
@@ -1,7 +1,54 @@
-while True:
-    input_expr = input("\n[in]: ")
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+operator = ['+', '-', '*', '/', '=']
+
+print("OrdinaryCAS beta\nOther features coming soon")
+
+         
+#expression lexer function
+def lexer(input_str):
+    input_str = input_str + " "
+    out_char = ""
+    expression = []
+    tokens = []
+
+    for x in range(len(input_str)):
+        char = input_str[x]
+        if char.isalpha():
+            out_char = out_char+char
+
+        elif char.isdigit():
+            out_char = out_char+char
+
+        elif char == "+" or char == "-" or char == "*" or char == "/" or char == "^" or char == "=":
+            expression.append(out_char)
+            out_char = ""
+            out_char = out_char+char
+            expression.append(out_char)
+            out_char=""
+        
+        elif char.isspace():
+            expression.append(out_char)
+            out_char = ""
+
+        
+
+
+
+    return expression
+
+
+#solver function
+def solve(string):
+    input_expr = string
     limit = int(input("limit: "))
-    expr = input_expr.split("=")
+    try:
+        expr = input_expr.split("=")
+    except IndexError:
+        print("input must be an equation. expressions will raise an error")
+        input_expr = input_expr + "=0"
+        expr = input_expr.split("=")
+    
     side_one = []
     side_two = []
     for x in range(-(limit), limit):
@@ -22,6 +69,30 @@ while True:
         if side_one[i] == side_two[i]:
             sol.append(side_one[i][0])
     
-    print("[out]: "+str(sol))
-        
+    return sol
+
+
+#function that converts mathematically natural language to computer understandable text
+def naturalise(input_str):
+    output = input_str
+    output_string = ""
+    for x in range(len(output)):
+        element = output[x]
+        for i in range(len(element)):
+            char = element[i]
+            if char in alpha and element[i-1] not in number:
+                output_string = output_string + "x"
+
+            if char in alpha and element[i-1] in number:
+                output_string = output_string + "*x"
+            
+            if char in number:
+                output_string = output_string + char
+
+            if char in operator:
+                output_string = output_string + char
+
+            if char == "^":
+                output_string = output_string + "**"
     
+    return output_string
